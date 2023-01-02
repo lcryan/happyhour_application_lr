1bbac3ea85576cd414fc398944b2e6608de1ab5d
LuciaRyan <lcryan06@gmail.com>
2023-01-02 12:23:44 +0100

    Imported Router in index.js Routing set up in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/home/Home';
+import Latest from './pages/latest/Latest';
+import Login from './pages/login/Login';
+import Mixologist from './pages/mixologist/Mixologist';
+import Randomizer from './pages/randomizer/Randomizer';
+import Registration from './pages/registration/Registration';
+import Search from './pages/search/Search';
+import TopTen from './pages/topten/TopTen';
+
 
 import './App.css';
 
 function App() {
-  return (
-    <>
-    <h1>Does this work in montserrat ?? </h1>
-    </>
-  );
+    return (
+        <>
+            {/* Future navigation */}
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/latest" element={<Latest/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/mixologist" element={<Mixologist/>}/>
+                <Route path="/randomizer" element={<Randomizer/>}/>
+                <Route path="/registration" element={<Registration/>}/>
+                <Route path="/search" element={<Search/>}/>
+                <Route path="/topten" element={<TopTen/>}/>
+            </Routes>
+
+
+        </>
+    );
 }
 
 export default App;
