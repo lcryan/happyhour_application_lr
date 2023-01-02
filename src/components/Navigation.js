7573c81aeec2f43b554b39a79f2747ef04ff3d9d
LuciaRyan <lcryan06@gmail.com>
2023-01-02 15:34:12 +0100

    navigation bar rudimental set up

---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navigation.css'
+
+function Navigation() {
+    return (
+
+        <>
+<nav className="navbar">
+    <ul className="list-searchbar">
+        <li>Home</li>
+        <li>Favorites</li>
+        <li>My Account</li>
+        <li>Help</li>
+    </ul>
+    <input className="search-bar" placeholder="Search your cocktail here">
+    </input>
+</nav>
+
+
+        </>
+    );
+}
+
+export default Navigation;
