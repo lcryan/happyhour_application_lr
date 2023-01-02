49db5be6ed537c39a3e4f8d51d81a7372d32142f
LuciaRyan <lcryan06@gmail.com>
2023-01-02 16:24:05 +0100

    added new image to project - test image only new article built - within this article different buttons are gonna be added in the future to mixologist, topten, latest cocktails

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import Navigation from "../../components/Navigation";
-import LogoImage from "../../assets/logo/TestLogo.png";
-import './Home.css'
 
-function Home(props) {
+import './Home.css'
+import LogoImage from "../../assets/logo/TestLogo.png";
+import Blue from "../../assets/BlueHawaiiCocktail.png";
+
+
+function Home() {
     return (
         <>
 
             <Navigation/>
             <header className="homepage-header">
-                <h1 className="header-homepage"> Welcome to your HappyHour</h1>
-                <img className="logo-image" src={LogoImage}></img>
-                <h2>Let's drink to that.</h2>
+                <img className="logo-image" src={LogoImage} alt="logo-image"/>
+                <h1 className="header-homepage"> Welcome to your HappyHour.</h1>
             </header>
+                <section className="overview-section">
+                    <article className="overview-article">
+                        <p>Blue Hawaii</p>
+                        <img className="day-cocktail-image" src={Blue}></img>
+                        <button className="randomizer-button" type='button'>Randomizer</button>
+
+                    </article>
+                </section>
+
+
         </>
     );
 }
