37826bb539f8406542fb062edbd7f9960b46e97e
LuciaRyan <lcryan06@gmail.com>
2023-01-02 15:33:32 +0100

    Home.js and Home.css : set up homepage header and navbar within homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Navigation from "../../components/Navigation";
+import LogoImage from "../../assets/logo/TestLogo.png";
+import './Home.css'
 
 function Home(props) {
     return (
-        <div>
-            <h1>This is my homepage</h1>
-        </div>
+        <>
+
+            <Navigation/>
+            <header className="homepage-header">
+                <h1 className="header-homepage"> Welcome to your HappyHour</h1>
+                <img className="logo-image" src={LogoImage}></img>
+                <h2>Let's drink to that.</h2>
+            </header>
+        </>
     );
 }
 
