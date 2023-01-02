0feaa7908cab50bfa33d56e3d59ec0b77cafe9e5
LuciaRyan <lcryan06@gmail.com>
2023-01-02 17:30:36 +0100

    alignment buttons on the right side still tbs

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -15,16 +15,19 @@ function Home() {
                 <img className="logo-image" src={LogoImage} alt="logo-image"/>
                 <h1 className="header-homepage"> Welcome to your HappyHour.</h1>
             </header>
-                <section className="overview-section">
-                    <article className="overview-article">
-                        <p>Blue Hawaii</p>
-                        <img className="day-cocktail-image" src={Blue}></img>
-                        <button className="randomizer-button" type='button'>Randomizer</button>
-
-                    </article>
-                </section>
-
-
+            <section className="overview-section">
+                <article className="random-cocktail-article">
+                    <p className="cocktail-name">Blue Hawaii</p>
+                    <img className="day-cocktail-image" src={Blue}/>
+                    <button className="randomizer-button" type='button'>Randomizer</button>
+                </article>
+                <article className="menu-buttons">
+                    <button className="latest">Latest Cocktails</button>
+                    <button className="top-ten">Our Top Ten</button>
+                    <p>Need more help?</p>
+                    <button className=""> Go to our MIXOLOGIST</button>
+                </article>
+            </section>
         </>
     );
 }
