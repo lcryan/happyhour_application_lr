57c74ee360b647712678162d157811e93ecab2f4
LuciaRyan <lcryan06@gmail.com>
2023-01-03 11:25:28 +0100

    added a link back to home on not found page

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function NotFound() {
     return (
         <>
             <h1>Ups...sorry, page not found. Go back home here.</h1>
+
+            <article>
+                <Link to="/">Go back Home</Link>
+            </article>
+
         </>
     );
 }
