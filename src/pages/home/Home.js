35df3b2b4aab01a12c8c74c851ca5662296fb99a
LuciaRyan <lcryan06@gmail.com>
2023-01-03 12:13:48 +0100

    all buttons lead to the subsequent pages. note: logout button still has to be added.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,28 +4,35 @@ import React from 'react';
 import './Home.css'
 import LogoImage from "../../assets/logo/TestLogo.png";
 import Blue from "../../assets/BlueHawaiiCocktail.png";
+import {useNavigate} from 'react-router-dom';
 
 
 function Home() {
+
+    const navigate = useNavigate()
+
     return (
+
         <>
-
-
             <header className="homepage-header">
-                <img className="logo-image" src={LogoImage} alt="logo   -image"/>
+                <img className="logo-image" src={LogoImage} alt="image of tha happy hour logo"/>
                 <h1 className="header-homepage"> Welcome to your HappyHour.</h1>
             </header>
             <section className="overview-section">
                 <article className="random-cocktail-article">
                     <p className="cocktail-name">Blue Hawaii</p>
-                    <img className="day-cocktail-image" src={Blue}/>
-                    <button className="randomizer-button" type='button'>Randomizer</button>
+                    <img className="day-cocktail-image" alt="image of blue hawaii cocktail" src={Blue}/>
+                    <button className="randomizer-button" onClick={() => navigate("/randomizer")}
+                            type='button'>Randomizer
+                    </button>
                 </article>
                 <article className="menu-buttons">
-                    <button className="latest">Latest Cocktails</button>
-                    <button className="top-ten">Our Top Ten</button>
+                    <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails</button>
+                    <button className="top-ten-button" onClick={() => navigate("/topten")}>Our Top Ten</button>
                     <p>Need more help?</p>
-                    <button className=""> Go to our MIXOLOGIST</button>
+                    <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
+                        MIXOLOGIST
+                    </button>
                 </article>
             </section>
         </>
