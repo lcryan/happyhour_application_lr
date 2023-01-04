57fb4b666e3f7cc5b1b1de5ebe20e984f7bb2b9f
LuciaRyan <lcryan06@gmail.com>
2023-01-04 14:18:56 +0100

    overview section items positioned - styling buttons in progress

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 import './Home.css'
 import LogoImage from "../../assets/logo/TestLogo.png";
 import Blue from "../../assets/BlueHawaiiCocktail.png";
-import Line from "../../assets/pngfind.com-page-divider-png-929139.png"
+
 import {useNavigate} from 'react-router-dom';
 
 
@@ -16,15 +16,14 @@ function Home() {
 
         <>
             <header className="homepage-header">
-                <img className="logo-image" src={LogoImage} alt="image of tha happy hour logo"/>
+                <img className="logo-image" src={LogoImage} alt="the happy hour logo"/>
                 <h1 className="header-homepage"> Welcome to your HappyHour.</h1>
-                <img className="page-divider" alt="page divider line black" src={Line}/>
             </header>
 
             <section className="overview-section">
                 <article className="random-cocktail-article">
                     <p className="cocktail-name">Blue Hawaii</p>
-                    <img className="day-cocktail-image" alt="image of blue hawaii cocktail" src={Blue}/>
+                    <img className="day-cocktail-image" alt="illustration blue hawaii cocktail" src={Blue}/>
                     <button className="randomizer-button" onClick={() => navigate("/randomizer")}
                             type='button'>Randomizer
                     </button>
@@ -32,7 +31,6 @@ function Home() {
                 <article className="menu-buttons">
                     <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails</button>
                     <button className="top-ten-button" onClick={() => navigate("/topten")}>Our Top Ten</button>
-                    <p>Need more help?</p>
                     <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
                         MIXOLOGIST
                     </button>
