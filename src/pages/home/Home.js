d7b2439c31918e587cb98ac46d7abe95a4c5c8d1
LuciaRyan <lcryan06@gmail.com>
2023-01-04 15:11:08 +0100

    styling homepage almost complete background of logo still has to be removed divider line missing as well (to be designed)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -29,6 +29,7 @@ function Home() {
                     </button>
                 </article>
                 <article className="menu-buttons">
+                    <p className="help-text">Choose to your liking from below...</p>
                     <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails</button>
                     <button className="top-ten-button" onClick={() => navigate("/topten")}>Our Top Ten</button>
                     <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
