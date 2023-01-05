021ed884dca3796b06d10226a88c5e810e28c2d7
LuciaRyan <lcryan06@gmail.com>
2023-01-05 11:20:33 +0100

    Started API call - data extraction in console succesful - call on data.drinks unsuccessful - cannot find the layer apparently - try to fix via OneCocktail component and combine with map call.

--- src/pages/latest/Latest.js
+++ src/pages/latest/Latest.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {useParams} from "react-router-dom";
 
-function Latest(props) {
+
+function Latest() {
+
+    const [cocktails, setCocktails] = useState([])
+
+    useEffect(() => {
+
+        async function getCocktailData() {
+            try {
+                const response = await axios.get('https://www.thecocktaildb.com/api/json/v2/9973533/recent.php');
+                /*console.log(response.data.drinks);*/
+                /*setCocktails(response.data.drinks)*/
+
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        void getCocktailData();
+    }, [])
+
+
     return (
-        <div>
-            <h1>Latest cocktails</h1>
-        </div>
+        <>
+            <section className="outer-content-container-latest-cocktails">
+                <div className="inner-content-container-latest-cocktails">
+                    <h2>Latest Cocktails</h2>
+                    {/*<div className="latest-cocktails-article-container">
+                        {cocktails.map((cocktail) => {
+                            return (
+
+                                <h3>{cocktail.data.drinks.dateModified}</h3>
+
+
+                            )
+                        })}
+                    </div>*/}
+
+                </div>
+            </section>
+        </>
     );
 }
 
