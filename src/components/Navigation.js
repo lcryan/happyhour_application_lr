e75e0617b853138ba7f27b3b879f4442b800c256
LuciaRyan <lcryan06@gmail.com>
2023-01-14 14:40:36 +0100

    rearranged position of list elements in navbar styled logout button incl. shadow for more affordance

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,31 +2,38 @@ import React from 'react';
 import {useContext} from "react";
 import './Navigation.css'
 import {NavLink} from "react-router-dom";
+
 /*import {AuthContext} from "../context/AuthContext";*/
 
 
 function Navigation() {
 
-/*    const {auth, logout} = useContext(AuthContext);
+    /*    const {auth, logout} = useContext(AuthContext);
 
-    function handleLogout() {
-        logout()
-    }*/
+        function handleLogout() {
+            logout()
+        }*/
 
     return (
         <>
             <nav className="navbar">
-                <ul className="list-searchbar">
-                    <li><NavLink to="/" className={ ( { isActive } ) => isActive ? "link--active" : "link" }>Home</NavLink></li>
+                <ul className="list-navbar">
+                    <li><NavLink to="/" className={({isActive}) => isActive ? "link--active" : "link"}>Home</NavLink>
+                    </li>
                     <li><NavLink to="/favourites">Favourites</NavLink></li>
-                    <li><NavLink to ="/myAccount" className={({isActive})=> isActive ? "link--active" : "link" }>My Account</NavLink></li>
+                    <li><NavLink to="/myAccount" className={({isActive}) => isActive ? "link--active" : "link"}>My
+                        Account</NavLink></li>
                     <li><NavLink to="/help">Help</NavLink></li>
                 </ul>
-
-       <button type="logout-button">Logout</button> {/* still has to be styled */}
-
-                <input className="search-bar" placeholder="Search your cocktail here">
-                </input>
+                <ul className="navbar-elements-right">
+                    <li>
+                        <input className="search-bar" placeholder="Search your cocktail here">
+                        </input>
+                    </li>
+                    <li>
+                        <button className="logout-button" type="button">Logout</button>
+                    </li>
+                </ul>
             </nav>
         </>
     );
