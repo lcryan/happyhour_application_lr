facc9283149f061e05609109ec8fd26968e1cfe0
LuciaRyan <lcryan06@gmail.com>
2023-01-14 17:08:28 +0100

    very basic registration form styling added changed and added some classes for that

--- src/pages/registration/Registration.js
+++ src/pages/registration/Registration.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link, useNavigate} from "react-router-dom";
 import {useState} from "react";
 import axios from "axios";
+import './Registration.css'
 
 function Registration() {
 
@@ -38,50 +39,56 @@ function Registration() {
 
 
     return (
-        <main className="container-user-registration">
-            <h1 className="title-sign-up">Sign up here!</h1>
-            <p className="text-sign-up"> Join our community of Cocktail Aficionados in just a few seconds!</p>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email-field">
-                    e-mailadres
-                    <input
-                        type="email"
-                        id="email-field"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </label>
+        <main>
+            <article className="outer-container-registration-form">
+                <h1 className="title-sign-up">Sign up here!</h1>
+                <p className="text-sign-up"> Join our community of Cocktail Aficionados in just a few seconds!</p>
 
-                <label htmlFor="username-field">
-                    username
-                    <input
-                        type="name"
-                        id="name-field"
-                        name="name"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </label>
+                <div className="user-input-boxes">
+                    <form className="registration-form" onSubmit={handleSubmit}>
+                        <label htmlFor="email-field">
+                            e-mailadres
+                            <input
+                                type="email"
+                                id="email-field"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </label>
 
-                <label htmlFor="password-field">
-                    password:
-                    <input type="password"
-                           id="password-field"
-                           name="password"
-                           value={password}
-                           onChange={(e) => setPassword(e.target.value)}
-                    />
-                </label>
-                {error && <p className="error">This account already exists. Please try another email-adres.</p>}
-                <button
-                    type="submit"
-                    className="submit-button"
-                    disabled={loading}
-                > Sign up!
-                </button>
-            </form>
+                        <label htmlFor="username-field">
+                            username
+                            <input
+                                type="name"
+                                id="name-field"
+                                name="name"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </label>
 
+                        <label htmlFor="password-field">
+                            password:
+                            <input type="password"
+                                   id="password-field"
+                                   name="password"
+                                   value={password}
+                                   onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </label>
+
+                        {error && <p className="error">This account already exists. Please try another email-adres.</p>}
+                        <button
+                            type="submit"
+                            className="submit-button"
+                            disabled={loading}
+                        > Sign up!
+                        </button>
+                    </form>
+                </div>
+
+            </article>
             <p>Do you already have an account? Then you can log in <Link to={"/login"}>here</Link>!</p>
         </main>
     );
