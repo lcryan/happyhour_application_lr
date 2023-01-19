894f3e846d9fac19e208b050ad997ef42588f417
LuciaRyan <lcryan06@gmail.com>
2023-01-19 11:58:02 +0100

    made the searchbar on home into a component - it's working in the log - now have to set up a button within the searchbar and send with on click to the resultpage - this page has to be made from scratch still

---
+++ src/components/SearchBar.js
@@ -0,0 +1,48 @@
+import React, {useEffect} from 'react';
+import {useState} from "react";
+import axios from "axios";
+
+
+function SearchBar() {
+
+    const [searchResult, setSearchResult] = useState("");
+
+    useEffect(() => {
+
+        async function getSearchResult() {
+            try {
+                const response = await axios.get(`https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${searchResult}`)
+                console.log(response.data)
+                setSearchResult(searchResult);
+            } catch (error) {
+                console.log(error)
+            }
+            if (searchResult) {
+                setSearchResult(searchResult)
+            } else  {
+                console.log("We couldn't find your cocktail");
+            }
+        }
+
+        void getSearchResult()
+    }, [searchResult]);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchResult(e.target.value);
+    }
+
+    return (
+        <main>
+            <input
+                type="text"
+                placeholder="Search Cocktail here"
+                onChange={handleChange}
+                className="search-bar"
+            />
+        </main>
+    );
+}
+
+export default SearchBar;
+
