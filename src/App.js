193dfa4b1a4bc31c5b649b7776a20500143268cc
LuciaRyan <lcryan06@gmail.com>
2023-01-19 14:10:25 +0100

    added SearchPage to Routing App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import Latest from './pages/latest/Latest';
 import Login from './pages/login/Login';
 import Mixologist from './pages/mixologist/Mixologist';
 import Registration from './pages/registration/Registration';
-/*import Search from './pages/search/Search';*/
 import TopTwenty from "./pages/topTwenty/TopTwenty";
 import SingleCocktail from "./pages/singleCocktail/SingleCocktail";
 import Favourites from './pages/favourites/Favourites';
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-
+import SearchPage from "./pages/searchPage/SearchPage";
 
 import './App.css';
 import Help from "./pages/help/Help";
-
 
 import NotFound from "./pages/notFound/NotFound";
 import MyAccount from "./pages/myAccount/MyAccount";
@@ -36,13 +34,13 @@ function App() {
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/myAccount" element={isAuth ? <MyAccount/> : <p>NOT LOGGED IN</p>}/>
                 <Route path="/mixologist" element={<Mixologist/>}/>
-                {/* <Route path="/search" element={<Search/>}/>*/}
                 <Route path="/topTwenty" element={<TopTwenty/>}/>
                 <Route path="/singleCocktail/:id" element={<SingleCocktail/>}/>
                 <Route path="/favourites" element={<Favourites/>}/>
                 <Route path="/help" element={<Help/>}/>
                 <Route path="*" element={<NotFound/>}/>
                 <Route path="/myAccount" element={<MyAccount/>}/>
+                <Route path="/searchPage/:id" element={<SearchPage/>}/>
             </Routes>
             <Footer/>
         </div>
