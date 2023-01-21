cd8a738e1f46dbc22fc628de9523d7bb9488f7e8
LuciaRyan <lcryan06@gmail.com>
2023-01-21 10:46:34 +0100

    added comments to all urls additionally added two more urls for search db and base url db

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
-export const baseUrl = "https://frontend-educational-backend.herokuapp.com";
+export const baseUrl = "https://frontend-educational-backend.herokuapp.com"; // baseURL novi backend
 
+export const DB_BASE_URL = "https://www.thecocktaildb.com"; // base url for cocktail db
+
+export const DB_SEARCH_URL = "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s="; //search cocktail name url
