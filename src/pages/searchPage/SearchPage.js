80f7a677cd3396521a061e3621c30a6d5e2c8bf3
LuciaRyan <lcryan06@gmail.com>
2023-01-21 11:07:36 +0100

    setting up search ingredients search-bar on SearchPage ; maybe reverting to make an additional component with the ingrdient search-bar within, as it makes the code look cleaner.

--- src/pages/searchPage/SearchPage.js
+++ src/pages/searchPage/SearchPage.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import SearchBar from "../../components/SearchBar";
+import {DB_BASE_URL, DB_SEARCH_INGREDIENT_URL, DB_SEARCH_URL} from "../../constants";
+import axios from "axios";
+
 
 function SearchPage() {
+    const [ingredient, setIngredient] = useState("");
+    const [nameIngredient, setNameIngredient] = useState([])
+
+
+    useEffect(() => {
+        async function getIngredients() {
+
+            try {
+                const response = await axios.get(`${DB_SEARCH_INGREDIENT_URL}${ingredient}`)
+                console.log(response.data)
+                setIngredient(response.data.drinks)
+
+            } catch (error) {
+                console.log(error)
+            }
+            if (ingredient) {
+                setIngredient(ingredient);
+            } else {
+                console.log("Sorry, we couldn't find the ingredient you were looking for. Please try again.")
+            }
+
+        }
+
+        void getIngredients()
+    }, [ingredient])
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setIngredient(e.target.value);
+    };
+
+    const recentInput = () => {
+        setIngredient("");
+    }
+
+
     return (
-        <main>
+        <>
+            <SearchBar/>
 
-
-        </main>
+        </>
     );
 }
 
