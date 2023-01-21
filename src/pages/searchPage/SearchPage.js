0622d1e9330aabe3b4e756320a524ee19057c5d7
LuciaRyan <lcryan06@gmail.com>
2023-01-21 11:51:44 +0100

    SearchPage still will be adjusted to include a SearchIngredientBar.js -

--- src/pages/searchPage/SearchPage.js
+++ src/pages/searchPage/SearchPage.js
@@ -1,45 +1,8 @@
 import React, {useEffect, useState} from 'react';
-
 import SearchBar from "../../components/SearchBar";
-import {DB_SEARCH_INGREDIENT_URL} from "../../constants";
-import axios from "axios";
 
 
 function SearchPage() {
-    const [ingredient, setIngredient] = useState("");
-    const [nameIngredient, setNameIngredient] = useState([])
-
-
-    useEffect(() => {
-        async function getIngredients() {
-
-            try {
-                const response = await axios.get(`${DB_SEARCH_INGREDIENT_URL}${ingredient}`)
-                console.log(response.data)
-                setIngredient(response.data.drinks)
-
-            } catch (error) {
-                console.log(error)
-            }
-            if (ingredient) {
-                setIngredient(ingredient);
-            } else {
-                console.log("Sorry, we couldn't find the ingredient you were looking for. Please try again.")
-            }
-
-        }
-
-        void getIngredients()
-    }, [ingredient])
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        setIngredient(e.target.value);
-    };
-
-    const recentInput = () => {
-        setIngredient("");
-    }
 
 
     return (
