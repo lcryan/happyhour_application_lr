930db6e88abad11ab3d0fbade8d42199e638475c
LuciaRyan <lcryan06@gmail.com>
2023-01-21 11:01:06 +0100

    added search by ingredient URL including personal API

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,9 @@
-export const baseUrl = "https://frontend-educational-backend.herokuapp.com"; // baseURL novi backend
+export const baseUrl = "https://frontend-educational-backend.herokuapp.com"; // baseURL novi backend herokuapp
 
 export const DB_BASE_URL = "https://www.thecocktaildb.com"; // base url for cocktail db
 
-export const DB_SEARCH_URL = "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s="; //search cocktail name url
+export const DB_SEARCH_URL = "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s="; //search cocktail
+// by name including personal API
+
+export const DB_SEARCH_INGREDIENT_URL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="; //search cocktail /
+// by ingredient URL including personal API
