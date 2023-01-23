1185c48a103d4fbb83be4ab5d1eb2919ee8365c5
LuciaRyan <lcryan06@gmail.com>
2023-01-23 13:58:33 +0100

    arranged all items, included h1

--- src/pages/searchPage/SearchPage.js
+++ src/pages/searchPage/SearchPage.js
@@ -9,6 +9,8 @@ function SearchPage() {
     return (
 
         <>
+
+            <h1 className="title-search-page"> Search your heart out! </h1>
             <article className="outer-container-search-bars">
                 <div className="name-box">
                     <h4>Search by name</h4>
