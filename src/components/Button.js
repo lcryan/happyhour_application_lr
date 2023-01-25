42544910a428af0cfc3c8b6195f21bb2e267dbba
LuciaRyan <lcryan06@gmail.com>
2023-01-25 14:27:14 +0100

    added button component -TODO still has to be implemented

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Button({type, className, onClick, disabled, children}) {
+    return (
+        <button
+            type={type}
+            className={className}
+            onClick={onClick}
+            disabled={disabled}>
+            {children}
+        </button>
+    );
+}
+
+export default Button;
