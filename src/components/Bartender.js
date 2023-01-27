5735d341db0a1e4d1115972da3e6418a10d94130
LuciaRyan <lcryan06@gmail.com>
2023-01-27 20:20:18 +0100

    formatted code

--- src/components/Bartender.js
+++ src/components/Bartender.js
@@ -3,15 +3,9 @@ import Button from "./Button";
 import './Bartender.css'
 import PartyButton from '../../src/assets/buttonPictures/PartyButton.png'
 
-
-
-
-function Bartender(props) {
+function Bartender() {
     return (
-
-
         <div>
-
             <div className="button-select-group">
                 <Button
                     type={"button"} //TODO check which kind of button this has to be ? In all cases!
@@ -20,9 +14,7 @@ function Bartender(props) {
                     children="Party-Drink"
                     style={{backgroundImage: PartyButton}}
                     //TODO onlick functionality still has to be added and has to iterate through
-
                 />
-
                 <Button
                     type={"button"}
                     className="shot"
