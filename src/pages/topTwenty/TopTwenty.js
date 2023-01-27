8ee5eda7534fcc5739d02c672a8318ab2eb00f11
LuciaRyan <lcryan06@gmail.com>
2023-01-27 22:39:49 +0100

    unsuccessful first attempt to set Error messages: show up on screen, even though there is no error...

--- src/pages/topTwenty/TopTwenty.js
+++ src/pages/topTwenty/TopTwenty.js
@@ -8,6 +8,9 @@ import {Link} from "react-router-dom";
 function TopTwenty() {
 
     const [topTwenty, setTopTwenty] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
 
     useEffect(() => {
 
@@ -16,6 +19,8 @@ function TopTwenty() {
         async function getTopTwenty() {
 
             try {
+                setError(false);
+                setLoading(true);
                 const response = await axios.get(`https://www.thecocktaildb.com/api/json/v2/9973533/popular.php`,
                     {signal: controller.signal});
                 /* console.log(response)*/
@@ -23,8 +28,11 @@ function TopTwenty() {
 
             } catch (e) {
                 console.error(e);
+                setError(true); //TODO ask Scott how to implement more defensive code here.
             }
         }
+
+        setLoading(false); //TODO ask Scott where to set up the loading...
 
         void getTopTwenty();
         return function cleanup() {
@@ -35,24 +43,22 @@ function TopTwenty() {
 
     return (
         <>
-            <section className="outer-content-container-topTen-cocktails">
-                <div className="inner-content-container-topTen-cocktails">
-                    <h1 className="title-top-ten"> Our User's Most Popular cocktails: </h1>
-                    <div className="top-ten-cocktails-container">
-                        {topTwenty.map((cocktail) => {
-                            return (
-                                <article className="cocktail-details" key={cocktail.idDrink}>
-                                    <Link to={`/singleCocktail/${cocktail.idDrink}`}>
-                                        <OneCocktailCard
-                                            keyStr={cocktail.idDrink}
-                                            imgStr={cocktail.strDrinkThumb}
-                                            strDrink={cocktail.strDrink}
-                                        />
-                                    </Link>
-                                </article>
-                            )
-                        })}
-                    </div>
+            <section className="outer-container-top-twenty">
+                <h1 className="title-top-twenty"> Our User's Most Popular cocktails: </h1>
+                <div className="inner-container-top-twenty">
+                    {topTwenty.map((cocktail) => {
+                        return (
+                            <article className="cocktail-details" key={cocktail.idDrink}>
+                                <Link to={`/singleCocktail/${cocktail.idDrink}`}>
+                                    <OneCocktailCard
+                                        keyStr={cocktail.idDrink}
+                                        imgStr={cocktail.strDrinkThumb}
+                                        strDrink={cocktail.strDrink}
+                                    />
+                                </Link>
+                            </article>
+                        )
+                    })}
                 </div>
             </section>
         </>
