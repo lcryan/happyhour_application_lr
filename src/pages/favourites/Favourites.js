3d30cc8cfaa786e360312e4a70a3bce81c77deed
LuciaRyan <lcryan06@gmail.com>
2023-01-27 14:22:55 +0100

    beginning of function to store favourites in localStorage

--- src/pages/favourites/Favourites.js
+++ src/pages/favourites/Favourites.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Favourites() {
+
+    const [favoriteCocktail, setFavouriteCocktail] = useState();
+
+    useEffect(() => {
+        localStorage.setItem('dataKey', JSON.stringify(favoriteCocktail))
+    })
+
+
     return (
         <>
             <h1>User's Favourite's List</h1>
