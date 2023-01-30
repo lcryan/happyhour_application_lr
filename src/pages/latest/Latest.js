2368f276cf8ee792ee4056848714df398f24e818
LuciaRyan <lcryan06@gmail.com>
2023-01-30 13:27:41 +0100

    no major changes made - just formatted code and removed unnecessary icon import

--- src/pages/latest/Latest.js
+++ src/pages/latest/Latest.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios"
 import './Latest.css'
-
 import OneCocktailCard from "../../components/OneCocktailCard";
 import {Link} from "react-router-dom";
 
