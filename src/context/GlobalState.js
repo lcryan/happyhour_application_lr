56fb0b0627c24d57ec86d2347aa60975651a17df
LuciaRyan <lcryan06@gmail.com>
2023-01-31 13:52:06 +0100

    removed code that wasn't working and added new to do's and adding and removing cocktail function steps in the to do's - tbd

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,34 +1,62 @@
-import React, {createContext, useReducer,useEffect} from 'react';
-import AppReducer from "./AppReducer";
+import React, {createContext} from 'react';
 
-// initial state
+
+
 
 const initialState = {
     favouritesList: [],
     addedToFavourites: [],
 }
 
-// create GlobalState
-
 export const GlobalContext = createContext(initialState);
 
 // create provider components
 export const GlobalProvider = (props) => {
-    const [state, dispatch] = useReducer(AppReducer,initialState);
+    const favourites = []
 
-    //actions
+    // TODO get the list of favourites from localstorge
+    // if it exists
+    // if it doesnt exist initialise empty array
 
-    const addCocktailToFavourites = cocktailData => {
-        dispatch({type: "ADD_COCKTAIL_TO_FAVOURITES", payload: cocktailData})
+
+    // TODO create function isFavourite()
+    // which checks the favourites array to
+    // see if the requested cocktail is favourited already or not
+    // export function as part of context to be used in other places
+
+
+    // TODO implement toggleFavoutire() function that combines the other functions into an
+    // easy to use function for use in event handlers on clicks
+
+
+    const addToFavourites = (cocktail) => {
+        // Check if cocktail is in favourites already
+
+        // if already favourite do nothing
+
+        // if not in favourites ad to favourites and update local storage
+        console.log("add")
+        console.log(cocktail)
+        favourites.push(cocktail)
+        console.log(favourites)
     }
+
+    const removeFromFavourites = (cocktail) => {
+        // loop through array and delete (loop, map, filter)
+        // save to localstorage
+        console.log("remove")
+        console.log(cocktail)
+    }
+
+    const contextData = {
+        addToFavourites,
+        removeFromFavourites,
+        favourites
+    };
 
     return (
         <GlobalContext.Provider
-            value={{
-                favouritesList: state.favouritesList,
-                addedToFavourites: state.addedToFavourites,
-                addCocktailToFavourites
-            }}>
+            value={contextData}>
             {props.children}
         </GlobalContext.Provider>
 
