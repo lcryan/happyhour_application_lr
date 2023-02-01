13483f8db333eee4baf1cfe45a9338a63924a434
LuciaRyan <lcryan06@gmail.com>
2023-02-01 19:25:36 +0100

    started restyling page on App.js for locals Home.js gradient change - purple tones Home.css implemented text color change colours might have to be reconsidered - optional lightgrey def works

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import './Home.css'
-import LogoImage from "../../assets/logo/TestLogo.png";
+import LogoImage from "../../assets/logo/Logo HappyHour App_lightgrey.svg";
 
 import {useNavigate} from 'react-router-dom';
 import RandomisedCocktail from "../../components/RandomisedCocktail";
@@ -17,26 +17,27 @@ function Home() {
 
                 <article className="homepage-title">
                     <img className="logo-image" src={LogoImage} alt="the happy hour logo"/>
-                    <h1 className="header-homepage"> Welcome to your HappyHour.</h1>
+                    <h1 className="header-homepage"> Welcome to your HappyHour!</h1>
                 </article>
 
                 <div className="inner-container-homepage">
-                <article className="overview-container">
-                    <article className="random-cocktail-article">
-                        <div className="one-random-cocktail">
-                            <RandomisedCocktail/>
-                        </div>
+                    <article className="overview-container">
+                        <article className="random-cocktail-article">
+                            <div className="one-random-cocktail">
+                                <RandomisedCocktail/>
+                            </div>
+                        </article>
+                        <article className="menu-buttons">
+                            <p className="help-text">Choose to your liking from below...</p>
+                            <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails
+                            </button>
+                            <button className="top-ten-button" onClick={() => navigate("/topTwenty")}>Our User's Top 20
+                            </button>
+                            <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
+                                MIXOLOGIST
+                            </button>
+                        </article>
                     </article>
-                    <article className="menu-buttons">
-                        <p className="help-text">Choose to your liking from below...</p>
-                        <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails</button>
-                        <button className="top-ten-button" onClick={() => navigate("/topTwenty")}>Our User's Top 20
-                        </button>
-                        <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
-                            MIXOLOGIST
-                        </button>
-                    </article>
-                </article>
                 </div>
             </div>
         </>
