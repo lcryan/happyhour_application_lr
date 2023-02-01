940381d71e8fa7430fef1a190ee62fbd90bfd78a
LuciaRyan <lcryan06@gmail.com>
2023-02-01 12:23:35 +0100

    see previous comment and commit on this

--- src/components/OneCocktailCard.js
+++ src/components/OneCocktailCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './OneCocktailCard.css'
 import AddToFavourites from "./AddToFavourites";
 
-function OneCocktailCard({ children, cocktail}) {
+function OneCocktailCard({children, cocktail}) {
     return (
         <>
             <article className="latest-cocktail"
                      key={cocktail.idDrink}>
-                <img src={cocktail.strDrinkThumb} alt="foto of according cocktail"
-                     className="cocktail-foto"/>
+                    <img src={cocktail.strDrinkThumb} alt="foto of according cocktail"
+                         className="cocktail-foto"/>
                 <AddToFavourites cocktail={cocktail}/>
                 {/*!// TODO result here - so all OneCocktailCards show up overlay */}
                 <p>{cocktail.strDrink}</p>
