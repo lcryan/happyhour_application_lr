f17776ab5d67b294d652803c113d9ca3e6509eef
LuciaRyan <lcryan06@gmail.com>
2023-02-02 14:13:25 +0100

    css finalized in home.css / home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import './Home.css'
 import LogoImage from "../../assets/logo/HapyHourLogo_Roger_brown.png";
+import DividerLine from "../../assets/icons/divider_line_straight_trudy_beige.svg"
+
 
 import {useNavigate} from 'react-router-dom';
 import RandomisedCocktail from "../../components/RandomisedCocktail";
@@ -18,27 +20,28 @@ function Home() {
                 <article className="header-title">
                     <img className="logo-image" src={LogoImage} alt="the happy hour logo"/>
                     <h1 className="title-homepage"> Welcome to your HappyHour!</h1>
+                    <img src={DividerLine} className="divider-line" alt="beige colored divider line"/>
                 </article>
 
-                <div className="inner-container-homepage">
-                    <article className="overview-container">
-                        <article className="random-cocktail-article">
-                            <div className="one-random-cocktail">
-                                <RandomisedCocktail/>
-                            </div>
-                        </article>
-                        <article className="menu-buttons">
-                            <p className="help-text">Choose to your liking from below...</p>
-                            <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails
-                            </button>
-                            <button className="top-ten-button" onClick={() => navigate("/topTwenty")}>Our User's Top 20
-                            </button>
-                            <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
-                                MIXOLOGIST
-                            </button>
-                        </article>
+
+                <article className="overview-container">
+                    <article className="random-cocktail-article">
+                        <div className="one-random-cocktail">
+                            <RandomisedCocktail/>
+                        </div>
                     </article>
-                </div>
+                    <article className="menu-buttons">
+                        <p className="help-text">Choose to your liking from below...</p>
+                        <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails
+                        </button>
+                        <button className="top-ten-button" onClick={() => navigate("/topTwenty")}>Our User's Top 20
+                        </button>
+                        <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
+                            MIXOLOGIST
+                        </button>
+                    </article>
+                </article>
+
             </div>
         </>
     );
