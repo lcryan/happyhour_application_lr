31c309c5a91811bf9848fd89bd097e1dbabcfdeb
LuciaRyan <lcryan06@gmail.com>
2023-02-02 14:14:55 +0100

    removed addtofavourites component - obsolete here

--- src/components/OneCocktailCard.js
+++ src/components/OneCocktailCard.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './OneCocktailCard.css'
-import AddToFavourites from "./AddToFavourites";
+
 
 function OneCocktailCard({children, cocktail}) {
     return (
         <>
             <article className="latest-cocktail"
                      key={cocktail.idDrink}>
-                    <img src={cocktail.strDrinkThumb} alt="foto of according cocktail"
-                         className="cocktail-foto"/>
-                <AddToFavourites cocktail={cocktail}/>
-                {/*!// TODO result here - so all OneCocktailCards show up overlay */}
+                <img src={cocktail.strDrinkThumb} alt="foto of according cocktail"
+                     className="cocktail-foto"/>
                 <p>{cocktail.strDrink}</p>
                 {children}
             </article>
