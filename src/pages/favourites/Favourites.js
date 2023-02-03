afd9e5e2fa7998507d9316b494fd22aa1973da62
LuciaRyan <lcryan06@gmail.com>
2023-02-03 14:26:23 +0100

    restyled Favourites Page - cocktails are aligned - checkboxes form have been added - text box for user added as well. Function for checkboxes and button still to be added

--- src/pages/favourites/Favourites.js
+++ src/pages/favourites/Favourites.js
@@ -3,8 +3,9 @@ import {AuthContext} from "../../context/AuthContext";
 import {GlobalContext} from "../../context/GlobalState";
 import {Link} from "react-router-dom";
 import OneCocktailCard from "../../components/OneCocktailCard";
+import './Favourites.css'
 
-function Favourites(props) {
+function Favourites() {
 
     const {user, isAuth} = useContext(AuthContext);
     const {favourites} = useContext(GlobalContext);
@@ -18,21 +19,53 @@ function Favourites(props) {
             return drinks.map((cocktail) => {
                 return (
                     <article className="cocktail-info" key={cocktail.idDrink}>
-                        <Link to={`/singleCocktail/${cocktail.idDrink}`}>
-                            <OneCocktailCard cocktail={cocktail} />
+                        <form className="one-fav"><input className="recipe-checkbox" type="checkbox"
+                                                         id="recipe-checkbox" value="recipe-checkbox"/>
+                            <label htmlFor="recipe-checkbox"> Recipe please </label>
+                        </form>
+                        <Link to={`/singleCocktail/${cocktail.idDrink}`}> <OneCocktailCard cocktail={cocktail}/>
                         </Link>
                     </article>
-
                 )
             })
+
         }
     }
+
     return (
         <>
+
             {isAuth ?
-                <h1>Welcome {user.username}, your favourites are: {getDrinks(favourites)}</h1>
+                <div className="header-favs">
+                    <h1 className="title-favs">Welcome {user.username}!</h1>
+                    <h2 className="subtitle-favs">Your favs are:</h2>
+                </div>
                 :
-                <p>You are not logged in.</p>
+                <h2 className="not-logged-in-message">You are not logged in.</h2>
+            }
+            {isAuth ?
+                <article className="favs-container"> {getDrinks(favourites)}
+                </article>
+                :
+                <article className="message-user">
+                    <h3>If you have an account: </h3>
+                    <h3>please sign in.</h3>
+                </article>
+
+            }
+
+            {isAuth ?
+                <div className="text-box-favs">
+                    <p className="important-note-text">Please remember: <p>you can save up
+                        to <strong>10</strong> cocktails
+                        on your favourites
+                        page.</p> <p>Choose <strong>3</strong> cocktails at a time to view the full recipe.Cheers!</p>
+                    </p>
+                </div> :
+                <div className="message-two-user">
+                    <p>Why you should join us!</p>
+                    <p>Not only have we 2000 cocktails</p>
+                </div>
             }
         </>
     );
