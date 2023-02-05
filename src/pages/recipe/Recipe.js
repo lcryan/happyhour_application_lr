3be89c9fe8c89fc344e73234ab570da2fe16a9d5
LuciaRyan <lcryan06@gmail.com>
2023-02-05 15:21:23 +0100

    favourites recipe page css styling finalized

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import axios from "axios";
+import "./Recipe.css";
 
 
 function Recipe() {
 
     const {id} = useParams();
     const [recipes, setRecipes] = useState([]);
-    const ids = id.split(',');
+    const ids = id.split(",");
 
     useEffect(() => {
 
@@ -38,18 +39,41 @@ function Recipe() {
         return function cleanup() {
             controller.abort();
         }
-    }, [])
+    }, [ids])
 
 
     return (
         <>{
             recipes.map((cocktail) => {
                 return (
-                    <article className="cocktail-info" key={cocktail.idDrink}>
-                        <h2>{cocktail.strDrink}</h2>
-                        <p className="test-recipe-text">{cocktail.strInstructions}</p>
+                    <article className="outer-container-recipe">
+                        <article className="inner-container-cocktail-info-recipe" key={cocktail.idDrink}>
+                            <div className="image-cocktail-recipe">
+                                <img src={cocktail.strDrinkThumb} className="recipe-image"
+                                     alt="foto of chosen cocktail"/>
+                            </div>
+                            <div className="container-name-and-ingredients">
+                                <div className="recipe-cocktail-name">
+                                    <h2>{cocktail.strDrink}</h2>
+                                </div>
+                                <ul className="list-of-ingredients-recipe">
+                                    <li>{cocktail.strIngredient1} {cocktail.strMeasure1}</li>
+                                    <li>{cocktail.strIngredient2} {cocktail.strMeasure2}</li>
+                                    <li>{cocktail.strIngredient3} {cocktail.strMeasure3}</li>
+                                    <li>{cocktail.strIngredient4} {cocktail.strMeasure4}</li>
+                                    <li>{cocktail.strIngredient5} {cocktail.strMeasure5}</li>
+                                    <li>{cocktail.strIngredient6} {cocktail.strMeasure6}</li>
+                                    <li>{cocktail.strIngredient7} {cocktail.strMeasure7}</li>
+                                    <li>{cocktail.strIngredient8} {cocktail.strMeasure8}</li>
+                                    <li>{cocktail.strIngredient9} {cocktail.strMeasure9}</li>
+                                    <li>{cocktail.strIngredient10} {cocktail.strMeasure10}</li>
+                                </ul>
+                            </div>
+                            <div className="container-text">
+                                <p className="recipe-instructions">{cocktail.strInstructions}</p>
+                            </div>
+                        </article>
                     </article>
-
                 )
             })
         }</>
