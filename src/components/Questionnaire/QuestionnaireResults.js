38c527f3eea6b94bad317f65cef566ce72c901d7
LuciaRyan <lcryan06@gmail.com>
2023-02-06 09:56:13 +0100

    styling results complete - issue - mixologist and questionnaire result are all on one page - header doesn't apply anymore, but still shows up in the results area, considering to add another routing to the QuestionnaireResults.js

--- src/components/Questionnaire/QuestionnaireResults.js
+++ src/components/Questionnaire/QuestionnaireResults.js
@@ -2,6 +2,10 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Link} from "react-router-dom";
 import OneCocktailCard from "../OneCocktailCard";
+import "./QuestionnaireResults.css";
+import DividerLine from "../../assets/icons/dividerline.svg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleArrowLeft} from "@fortawesome/free-solid-svg-icons/faCircleArrowLeft";
 
 function QuestionnaireResults(props) {
 
@@ -11,39 +15,60 @@ function QuestionnaireResults(props) {
     useEffect(() => {
 
         async function fetchCocktails() {
-
-            const filterQuery = `${filters.join('&')}`
-            console.log(filterQuery)
-            const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?${filterQuery}`)
-            console.log(response)
-            setResult(response.data.drinks.slice(0, 10))
+            try {
+                const filterQuery = `${filters.join('&')}`
+                console.log(filterQuery)
+                const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?${filterQuery}`)
+                console.log(response)
+                setResult(response.data.drinks.slice(0, 10))
+            } catch (error) {
+                console.log(error)
+            }
         }
 
-        try {
-            fetchCocktails()
-        } catch (error) {
-            console.log(error)
-        }
-    }, [])
+        void fetchCocktails();
+    },);
 
-    if (!result) {
-        return "Sorry, we couldn't find your cocktail! Please try again!"
+    const getResults = (drinks) => {
+        if (!drinks.length || drinks.length === 0) {
+            return (
+                <>
+                    <p>No cocktails found.</p>
+                </>
+            )
+        } else {
+            return (result.map((cocktail) => {
+                return (
+                    <>
+                        <article className="cocktail-details" key={cocktail.idDrink}>
+                            <Link to={`/singleCocktail/${cocktail.idDrink}`}>
+                                <OneCocktailCard
+                                    cocktail={cocktail}
+                                />
+                            </Link>
+                        </article>
+                    </>
+                )
+            }))
+        }
     }
 
-    return (result.map((cocktail) => {
-        return (
-            <>
-                <article className="cocktail-details" key={cocktail.idDrink}>
-                    <Link to={`/singleCocktail/${cocktail.idDrink}`}>
-                        <OneCocktailCard
-                            cocktail={cocktail}
-                        />
-                    </Link>
-                </article>
+    return (
+        <>
+            <div className="header-questionnaire">
+                <h1>This is what we found for you</h1>
+                <img src={DividerLine} className="divider-line-questionnaire" alt="beige thin element divider"/>
+            </div>
+            <article className="questionnaire-cocktails-container">
+                {getResults(result)}
+            </article>
 
-            </>
-        )
-    }))
+            <div className="back-arrow-box-questionnaire">
+                <Link to="/"><FontAwesomeIcon icon={faCircleArrowLeft} className="back-arrow-questionnaire"/></Link>
+            </div>
+        </>
+
+    );
 }
 
 export default QuestionnaireResults;
