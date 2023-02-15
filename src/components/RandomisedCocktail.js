d96c113c0bc4775ff8d5daf36effd1c048335ce0
LuciaRyan <lcryan06@gmail.com>
2023-02-15 09:52:25 +0100

    added spinner to RandomisedCocktail.js styled Spinner.css / Spinner.js

--- src/components/RandomisedCocktail.js
+++ src/components/RandomisedCocktail.js
@@ -3,24 +3,25 @@ import OneCocktailCard from "./OneCocktailCard";
 import axios from "axios";
 import {Link} from "react-router-dom";
 import './RandomisedCocktail.css'
+import Spinner from "./Spinner";
+import "./Spinner.css"
 
 function RandomisedCocktail() {
 
     const [randomCocktail, setRandomCocktail] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const apiUrl = `https://www.thecocktaildb.com/api/json/v2/9973533/random.php`;
 
     const fetchCocktailHandler = useCallback(() => {
-        setRandomCocktail({
-            strDrink: "loading",
-            idDrink: "",
-            strDrinkThumb: "https://via.placeholder.com/150"
-        })
+        setLoading(true) //set loading to true when fetching my data
+        setRandomCocktail(null)
 
         axios.get(apiUrl).then(response => {
             console.log(response.data)
             setRandomCocktail(response.data.drinks[0])
         }).catch(e => console.log(e))
+            .finally(() => setLoading(false)) //setLoading to false when there is an actual response
 
     }, []);
 
@@ -31,13 +32,15 @@ function RandomisedCocktail() {
 
 
     return (<>
-
-        <article className="cocktail-details" key={randomCocktail.idDrink}>
-            <Link to={`/singleCocktail/${randomCocktail.idDrink}`}>
-                <OneCocktailCard cocktail={randomCocktail}
-                />
-            </Link>
-        </article>
+        {loading ? ( // show the spinner when loading is true
+            <Spinner/>
+        ) : (
+            <article className="cocktail-details" key={randomCocktail.idDrink}>
+                <Link to={`/singleCocktail/${randomCocktail.idDrink}`}>
+                    <OneCocktailCard cocktail={randomCocktail}
+                    />
+                </Link>
+            </article>)}
         <button className="randomizer-button" onClick={() => {
             fetchCocktailHandler()
         }}
