53ffa024bf78e84a9005d85163140a87314893a2
LuciaRyan <lcryan06@gmail.com>
2023-02-15 10:03:15 +0100

    fromatted code

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,50 +1,53 @@
-import React from 'react';
+import React from "react";
 import "./About.css";
+
 function About() {
     return (
         <>
+            <div className="about-greeting">
+                <h1>
+                    About The Happy Hour Team
+                </h1>
+            </div>
 
+            <section className="container-about-us-texts">
+                <article className="our-mission-text">
+                    <h2>Our mission</h2>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                        there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the
+                        Semantics, a large language ocean. A small river named Duden flows by their place and
+                        supplies it with the necessary regelialia.</p>
+                    <p>It is a paradisematic country, in which roasted
+                        parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about
+                        the blind texts it is an almost unorthographic life One day however a small line of blind
+                        text by the name of Lorem Ipsum decided to</p>
+                </article>
 
-                <div className="about-greeting">
-                    <h1>
-                        About The Happy Hour Team
-                    </h1>
-                </div>
+                <article className="our-community-text">
+                    <h2>Our Community</h2>
+                    <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of
+                        spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in
+                        this spot, which was created for the bliss of souls like mine. </p>
+                    <p>I am so happy, my dear
+                        friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my
+                        talents. I should be incapable of drawing a single stroke at the present moment; and yet I
+                        feel that I never was a greater artist than now. When, while the lovely valley teems
+                        with</p>
+                </article>
 
-                <section className="container-about-us-texts">
-                    <article className="our-mission-text">
-                        <h2>Our mission</h2>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
-                            there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the
-                            Semantics, a large language ocean. A small river named Duden flows by their place and
-                            supplies it with the necessary regelialia. It is a paradisematic country, in which roasted
-                            parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about
-                            the blind texts it is an almost unorthographic life One day however a small line of blind
-                            text by the name of Lorem Ipsum decided to</p>
-                    </article>
+                <article className="our-services-text">
+                    <h2>Our Service</h2>
+                    <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his
+                        bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a
+                        little he could see his brown belly, slightly domed and divided by arches into stiff
+                        sections.</p>
+                    <p>The bedding was hardly able to cover it and seemed ready to slide off any moment.
+                        His many legs, pitifully thin compared with the size of the rest of him, waved about
+                        helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream. His room, a
+                        proper human</p>
+                </article>
+            </section>
 
-                    <article className="our-community-text">
-                        <h2>Our Community</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of
-                            spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in
-                            this spot, which was created for the bliss of souls like mine. I am so happy, my dear
-                            friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my
-                            talents. I should be incapable of drawing a single stroke at the present moment; and yet I
-                            feel that I never was a greater artist than now. When, while the lovely valley teems
-                            with</p>
-                    </article>
-
-                    <article className="our-services-text">
-                        <h2>Our Service</h2>
-                        <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his
-                            bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a
-                            little he could see his brown belly, slightly domed and divided by arches into stiff
-                            sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.
-                            His many legs, pitifully thin compared with the size of the rest of him, waved about
-                            helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream. His room, a
-                            proper human</p>
-                    </article>
-                </section>
         </>
     );
 }
