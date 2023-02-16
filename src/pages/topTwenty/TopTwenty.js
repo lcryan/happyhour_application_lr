79bc6681d1b6dc1118832e77941f44510cba44ea
LuciaRyan <lcryan06@gmail.com>
2023-02-16 12:21:14 +0100

    loading, errorMessages not working - tbs

--- src/pages/topTwenty/TopTwenty.js
+++ src/pages/topTwenty/TopTwenty.js
@@ -1,17 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import OneCocktailCard from '../../components/OneCocktailCard';
-import axios from 'axios'
-import './TopTwenty.css'
-import {Link} from 'react-router-dom';
-import DividerLine from '../../assets/icons/dividerline.svg'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCircleArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import React, {useEffect, useState} from "react";
+import OneCocktailCard from "../../components/OneCocktailCard";
+import axios from "axios";
+import "./TopTwenty.css";
+import {Link} from "react-router-dom";
+import DividerLine from "../../assets/icons/dividerline.svg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleArrowLeft} from "@fortawesome/free-solid-svg-icons";
 
 function TopTwenty() {
 
     const [topTwenty, setTopTwenty] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
 
     useEffect(() => {
@@ -21,20 +22,19 @@ function TopTwenty() {
         async function getTopTwenty() {
 
             try {
-                setError(false);
                 setLoading(true);
+                setError(false);
                 const response = await axios.get(`https://www.thecocktaildb.com/api/json/v2/9973533/popular.php`,
                     {signal: controller.signal});
-                /* console.log(response)*/
                 setTopTwenty(response.data.drinks)
 
             } catch (e) {
                 console.error(e);
-                setError(true); //TODO ask Scott how to implement more defensive code here.
+                setError(true);
             }
         }
 
-        setLoading(false); //TODO ask Scott where to set up the loading...loading messages didn't work
+        setLoading(false);
 
         void getTopTwenty();
         return function cleanup() {
@@ -49,20 +49,23 @@ function TopTwenty() {
                 <h1 className="title-top-twenty"> Our fans' faves...</h1>
                 <img src={DividerLine} className="divider-line-top-twenty" alt="beige-thin-line"/>
             </div>
-            <div className="container-top-twenty">
-                {topTwenty.map((cocktail) => {
-                    return (
-                        <article className="cocktail-details" key={cocktail.idDrink}>
-                            <Link to={`/singleCocktail/${cocktail.idDrink}`}>
-                                <OneCocktailCard cocktail={
-                                    cocktail
-                                }
-                                />
-                            </Link>
-                        </article>
-                    )
-                })}
-            </div>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="container-top-twenty">
+                    {topTwenty.map((cocktail) => {
+                        return (
+                            <article className="cocktail-details" key={cocktail.idDrink}>
+                                <Link to={`/singleCocktail/${cocktail.idDrink}`}>
+                                    <OneCocktailCard cocktail={
+                                        cocktail
+                                    }
+                                    />
+                                </Link>
+                            </article>
+                        )
+                    })}
+                </div>)}
             <div className="back-arrow-tt-box">
                 <Link to="/"><FontAwesomeIcon className="back-arrow-tt" icon={faCircleArrowLeft}/></Link>
             </div>
