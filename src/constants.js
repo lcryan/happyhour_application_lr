85c786e79d3bbe366ff87b8c2ff3d818b1fc60a8
LuciaRyan <lcryan06@gmail.com>
2023-02-16 14:00:32 +0100

    formatted code

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const BACKEND_BASE_URL = "https://frontend-educational-backend.herokuapp.com",
+export const
+    BACKEND_BASE_URL = "https://frontend-educational-backend.herokuapp.com",
     DB_SEARCH_URL = "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=",
     DB_SEARCH_INGREDIENT_URL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=",
     DB_SEARCH_BY_FIRST_LETTER_URL = "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?f=",
