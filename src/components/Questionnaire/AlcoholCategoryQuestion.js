af902fa5fa1c28e7b31445ea6b89f60036ff0c1a
LuciaRyan <lcryan06@gmail.com>
2023-02-16 13:17:49 +0100

    formatted

--- src/components/Questionnaire/AlcoholCategoryQuestion.js
+++ src/components/Questionnaire/AlcoholCategoryQuestion.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState} from "react";
 import Button from "../Button";
 import GlassCategoryQuestion from "./GlassCategoryQuestion";
 import "./CategoriesStyling.css";
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState} from "react";
 import axios from "axios";
 import {AuthContext} from "../../context/AuthContext";
 import {BACKEND_BASE_URL} from "../../constants";
