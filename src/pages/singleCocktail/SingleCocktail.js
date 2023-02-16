088bfd5930473b93bd19bba2076de8ad99a4dfe5
LuciaRyan <lcryan06@gmail.com>
2023-02-16 11:58:16 +0100

    loading functional and styled accordingly error and errorMessages still to be set - were non-functional

--- src/pages/singleCocktail/SingleCocktail.js
+++ src/pages/singleCocktail/SingleCocktail.js
@@ -1,17 +1,19 @@
-import './SingleCocktail.css';
+import "./SingleCocktail.css";
 import {useState, useEffect} from "react";
 import {Link, useParams} from "react-router-dom";
 import axios from "axios";
-import React from 'react';
+import React from "react";
 import AddToFavourites from "../../components/AddToFavourites";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faChevronCircleLeft} from '@fortawesome/free-solid-svg-icons';
+import {faChevronCircleLeft} from "@fortawesome/free-solid-svg-icons";
 
 
 function SingleCocktail() {
 
     const [oneCocktail, setOneCocktail] = useState({});
     const [loading, setLoading] = useState(false);
+    //TODO set error and errorMessage ?
+
     const {id} = useParams();
 
 
@@ -20,6 +22,8 @@ function SingleCocktail() {
         const controller = new AbortController();
 
         async function getOneCocktail() {
+
+            setLoading(true);
 
             try {
 
@@ -33,10 +37,9 @@ function SingleCocktail() {
                     setOneCocktail(null);
                     setLoading(false);
                 }
-            } catch (e) {
-                console.log(e)
+            } catch (error) {
+                console.log(error)
             }
-
         }
 
         void getOneCocktail();
@@ -47,51 +50,49 @@ function SingleCocktail() {
 
     }, [id]);
 
-    if (loading) {
-        return "Getting cocktail..."
-    }
-    if (!oneCocktail) {
-        return <h3>No cocktail to display</h3>
-    }
-
     return (
 
         <>
-            <section className="outer-container-single-cocktail">
-                <article className="container-content-single-cocktail">
-                    <>
-                        <div className="image-container">
-                            <p className="hover-text">Hover to add to/remove from favs</p>
-                            <img className="foto-singleCocktail" alt="foto of single-cocktail"
-                                 src={oneCocktail.strDrinkThumb}/>
+            {loading ? (<h3 className="loading-message-single">Loading...</h3>) : (
+                <section className="outer-container-single-cocktail">
+                    <article className="container-content-single-cocktail">
+                        <>
+                            <div className="image-container">
+                                <p className="hover-text">Hover to add to/remove from favs</p>
+                                <img className="foto-singleCocktail" alt="foto of single-cocktail"
+                                     src={oneCocktail.strDrinkThumb}/>
 
-                            <AddToFavourites cocktail={oneCocktail}/>
+                                <AddToFavourites cocktail={oneCocktail}/>
 
-                        </div>
-                        <div className="content-single-cocktail">
-                            <h3 className="single-cocktail-title">{oneCocktail.strDrink} </h3>
-                            <ul className="ingredients-list">
-                                Ingredients:
-                                <li className="ingredients"> {oneCocktail.strIngredient1}</li>
-                                <li className="ingredients"> {oneCocktail.strIngredient2} </li>
-                                <li className="ingredients"> {oneCocktail.strIngredient3} </li>
-                                <li className="ingredients"> {oneCocktail.strIngredient4} </li>
-                                <li className="ingredients">{oneCocktail.strIngredient5}</li>
-                                <li className="ingredients">{oneCocktail.strIngredient6}</li>
-                                <li className="ingredients">{oneCocktail.strIngredient7}</li>
-                                <li className="ingredients">{oneCocktail.strIngredient8}</li>
-                                <li className="ingredients">{oneCocktail.strIngredient9}</li>
-                                <li className="ingredients">{oneCocktail.strIngredient10}</li>
-                            </ul>
-                            <div className="back-arrow-single">
-                                <Link to="/"><FontAwesomeIcon className="chevron-arrow-left"
-                                                              icon={faChevronCircleLeft}/></Link>
                             </div>
-                        </div>
-                    </>
-                </article>
-            </section>
+
+                            <div className="content-single-cocktail">
+                                <h3 className="single-cocktail-title">{oneCocktail.strDrink} </h3>
+                                <ul className="ingredients-list">
+                                    Ingredients:
+                                    <li className="ingredients"> {oneCocktail.strIngredient1}</li>
+                                    <li className="ingredients"> {oneCocktail.strIngredient2} </li>
+                                    <li className="ingredients"> {oneCocktail.strIngredient3} </li>
+                                    <li className="ingredients"> {oneCocktail.strIngredient4} </li>
+                                    <li className="ingredients">{oneCocktail.strIngredient5}</li>
+                                    <li className="ingredients">{oneCocktail.strIngredient6}</li>
+                                    <li className="ingredients">{oneCocktail.strIngredient7}</li>
+                                    <li className="ingredients">{oneCocktail.strIngredient8}</li>
+                                    <li className="ingredients">{oneCocktail.strIngredient9}</li>
+                                    <li className="ingredients">{oneCocktail.strIngredient10}</li>
+                                </ul>
+                                <div className="back-arrow-single">
+                                    <Link to="/"><FontAwesomeIcon className="chevron-arrow-left"
+                                                                  icon={faChevronCircleLeft}/></Link>
+                                </div>
+                            </div>
+                        </>
+                    </article>
+                </section>
+            )}
+
         </>
+
     );
 }
 
