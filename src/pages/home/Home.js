7e1ccde02d21b9c6a2e597c8195abdbc74ebc2f8
LuciaRyan <lcryan06@gmail.com>
2023-02-18 09:32:42 +0100

    added missing header

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -15,34 +15,35 @@ function Home() {
 
     return (
         <>
-            <div className="outer-container-hompage">
-
-                <article className="header-title">
-                    <img className="logo-image" src={LogoImage} alt="the happy hour logo"/>
-                    <h1 className="title-homepage"> Welcome to your HappyHour!</h1>
-                    <img src={DividerLine} className="divider-line" alt="beige colored divider line"/>
-                </article>
-
-                <article className="overview-container">
-                    <article className="random-cocktail-article">
-                        <div className="one-random-cocktail">
-                            <RandomisedCocktail/>
-                        </div>
+            <main>
+                <header>
+                    <article className="header-title">
+                        <img className="logo-image" src={LogoImage} alt="the happy hour logo"/>
+                        <h1 className="title-homepage"> Welcome to your HappyHour!</h1>
+                        <img src={DividerLine} className="divider-line" alt="beige colored divider line"/>
                     </article>
-                    <article className="menu-buttons">
-                        <p className="help-text">Choose to your liking from below...</p>
-                        <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails
-                        </button>
-                        <button className="top-ten-button" onClick={() => navigate("/topTwenty")}>Our User's Top 20
-                        </button>
-                        <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
-                            MIXOLOGIST
-                        </button>
-                    </article>
-                </article>
 
-            </div>
+                    <article className="overview-container">
+                        <article className="random-cocktail-article">
+                            <div className="one-random-cocktail">
+                                <RandomisedCocktail/>
+                            </div>
+                        </article>
+                        <article className="menu-buttons">
+                            <p className="help-text">Choose to your liking from below...</p>
+                            <button className="latest-button" onClick={() => navigate("/latest")}>Latest Cocktails
+                            </button>
+                            <button className="top-ten-button" onClick={() => navigate("/topTwenty")}>Our User's Top 20
+                            </button>
+                            <button className="mixologist-button" onClick={() => navigate("/mixologist")}> Go to our
+                                MIXOLOGIST
+                            </button>
+                        </article>
+                    </article>
+                </header>
+            </main>
         </>
+
     );
 }
 
